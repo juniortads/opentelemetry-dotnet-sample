40347ce171ed3ac5926120d4bda117c7dfae6a7a
juniortads <junior.tads@gmail.com>
2021-01-18 14:59:54 -0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# opentelemetry-dotnet-sample
+# ASP.NET Core Instrumentation for OpenTelemetry .NET 
+
+It is a simple project created with ASP.NET Core that uses OpenTelemetry for instrumentation and telemetry collection on incoming web requests.
+
+## Getting started
+
+Run Jaeger
+
+>  docker-compose up -d
+
+Start the service
+
+> dotnet run
+
+## Advanced configuration
+
+This instrumentation can be configured to change the default behavior 
+
+### Enrich
+
+For this sample we can enrich with tags some very important **Headers["X-TENANT-ID"]**
+
+```csharp
+services.AddOpenTelemetryTracing((builder) => builder
+                   .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(Configuration.GetValue<string>("Jaeger:ServiceName")))
+                    .AddAspNetCoreInstrumentation((options) => options.Enrich = (activity, eventName, rawObject) =>
+                    {
+                        if (eventName.Equals("OnStartActivity"))
+                        {
+                            if (rawObject is HttpRequest httpRequest)
+                            {
+                                activity.SetTag("X-TENANT-ID", httpRequest.Headers["X-TENANT-ID"]);
+                            }
+                        }
+                    })
+                    .AddJaegerExporter(jaegerOptions =>
+                    {
+                        jaegerOptions.AgentHost = Configuration.GetValue<string>("Jaeger:Host");
+                        jaegerOptions.AgentPort = Configuration.GetValue<int>("Jaeger:Port");
+                    }));
+```
+
+## Exporter libraries
+
+I used Jaeger, but there are many others, see the [OpenTelemetry registry](https://opentelemetry.io/registry/?s=net) for more exporters.
+
---
+++ src/MicroservicesSample/Startup.cs
@@ -0,0 +1,74 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using OpenTelemetry;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+namespace MicroservicesSample
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "MicroservicesSample", Version = "v1" });
+            });
+
+            services.AddOpenTelemetryTracing((builder) => builder
+                    .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(Configuration.GetValue<string>("Jaeger:ServiceName")))
+                    .AddAspNetCoreInstrumentation((options) => options.Enrich = (activity, eventName, rawObject) =>
+                    {
+                        if (eventName.Equals("OnStartActivity"))
+                        {
+                            if (rawObject is HttpRequest httpRequest)
+                            {
+                                activity.SetTag("X-TENANT-ID", httpRequest.Headers["X-TENANT-ID"]);
+                            }
+                        }
+                    })
+                    .AddJaegerExporter(jaegerOptions =>
+                    {
+                        jaegerOptions.AgentHost = Configuration.GetValue<string>("Jaeger:Host");
+                        jaegerOptions.AgentPort = Configuration.GetValue<int>("Jaeger:Port");
+                    }));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MicroservicesSample v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
